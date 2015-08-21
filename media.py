dac5863a8cff4295465b785bc724de48fe9b1be9
rumblefish2494 <rumbelfish2494@gmail.com>
2015-08-20 21:52:05 -0700

    fixed pep8 errors

--- media.py
+++ media.py
@@ -1,15 +1,18 @@
 import webbrowser
 
-class Movie(): #class
+
+class Movie():  # class
     """this class provides a way to store movie related information."""
     VALID_RATINGS = ['P', 'PG', 'PG-13', 'R']
-    
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube): #constructor!
-        self.title = movie_title #instance variables
+    # define init (constructor) that creates instances of class Movie
+
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):  # constructor!
+        self.title = movie_title  # instance variables
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-        
-    def show_trailer(self): #instance methods
+
+    def show_trailer(self):  # instance methods
         webbrowser.open(self.trailer_youtube_url)
         print self.trailer
